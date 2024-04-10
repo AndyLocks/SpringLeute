a2a275d82ae14e2eecd0f985e22a1ad17fc7d760
AndyLocks <jandylokc@gmail.com>
2024-04-10 22:59:24 +0200

    Add spring config: redis pool and logging filter

---
+++ src/main/java/com/leute/spring_leute/SpringConfig.java
@@ -0,0 +1,28 @@
+package com.leute.spring_leute;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import redis.clients.jedis.JedisPool;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    @Scope("singleton")
+    JedisPool jedisPool() {
+        return new JedisPool("localhost", 6379);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setMaxPayloadLength(64000);
+
+        return loggingFilter;
+    }
+}
